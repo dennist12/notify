a3f70733720ef41e21e2c38ff2e3fbd38bc1f3a9
Dennis Tabaldo <tabaldodennis@gmail.com>
2019-05-16 02:52:50 +0800

    add card layout

--- Resource/views/notifications/index.blade.php
+++ Resource/views/notifications/index.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-       
-        <div class="row">
-            <div class="col">
-                <h1 >Notifications</h1>
-            </div>
-        </div>
+    <div class="container mt-4">
+        
         <div class="row justify-content-center">
-            <div class="col">
-                <ul class="list-group list-group-flush">
-                    @foreach ($notifications as $notification)
-                        <li class="list-group-item">{{ $notification }}</li>
-                    @endforeach
-                </ul>
-                
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                {{ $notifications->links() }}
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Notifications</h5>
+                    <ul class="list-group list-group-flush">
+                        @foreach ($notifications as $notification)
+                            <li class="list-group-item">
+                                @if(isset($notification['data']['content']['title']))
+                                    <strong>{{ $notification['data']['content']['title'] }}</strong>
+                                    {{ $notification['data']['content']['body'] }}
+                                @endif
+                            </li>
+                        @endforeach
+                    </ul>
+                   <div class="mt-2">
+                        {{ $notifications->links() }}
+                   </div>
+                </div>
             </div>
         </div>
     </div>
